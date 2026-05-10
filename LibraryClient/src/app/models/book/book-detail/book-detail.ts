d33d732e801754a0a8f02ece5dee5cb703294681
giovanniDalise <135616195+giovanniDalise@users.noreply.github.com>
2026-05-10 09:51:53 +0200

    Aggiunta altri campi dettaglio book

--- LibraryClient/src/app/models/book/book-detail/book-detail.ts
+++ LibraryClient/src/app/models/book/book-detail/book-detail.ts
@@ -1,8 +1,10 @@
 export interface BookDetail {
-    id: number;
-    title: string;
-    isbn: string;
-    coverReference: string;
-    coverFile: File | null;
-    // aggiungerei poi due dictionary nome e id per editor author per le tendine delle associazioni
+  id: number;
+  title: string;
+  isbn: string;
+  coverReference: string;
+  coverFile: File | null;
+  authorName: string;
+  authorSurname: string;
+  editorName: string;
 }
